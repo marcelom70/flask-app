d850370d125960aebd58cf342d860f05b72c270a
marcelom70 <marcelo.marcelom@gmail.com>
2026-04-07 13:32:40 -0300

    Description of the message was changed

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/send', methods=['POST'])
 def send():
     message = request.form['message']    
-    return f"Message sent to CI/CD application (after deploy and ssh protection): {message}"
+    return f"Message sent to flask application: {message}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
