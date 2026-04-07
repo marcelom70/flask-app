fcc4478bfa5937a710bbe1c787d85df33bac9635
marcelom70 <marcelo.marcelom@gmail.com>
2026-04-06 21:48:20 -0300

    Initial Flask + Docker setup

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/send', methods=['POST'])
+def send():
+    message = request.form['message']    
+    return f"Message sent to CI/CD application: {message}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
